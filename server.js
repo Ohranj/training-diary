4372ceb1e18abd2ce7a61c4a2523710d7504056f
Ohranj <ajdorrington@hotmail.co.uk>
2020-12-29 19:53:23 +0000

    Added login and register pages

--- server.js
+++ server.js
@@ -34,4 +34,14 @@ app.use(passport.session());
 //Serve routes
 app.use("/", jsonParser, authRoutes);
 
+//Heroku hosting
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "build", "index.html")
+        );
+    });
+}
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
