8a9a638bb6ad1cd3ba67f4bd6b454e077c942a3d
Ohranj <ajdorrington@hotmail.co.uk>
2020-12-29 20:09:13 +0000

    Added login and register pages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
 const { config } = require("dotenv");
+const path = require("path");
 config();
 
 const app = express();
