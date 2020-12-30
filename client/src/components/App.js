0ebab36abba337a68f878341de05bc3031a1a041
Ohranj <ajdorrington@hotmail.co.uk>
2020-12-30 14:49:18 +0000

    Added 404 page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../screens/home/Home";
 import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../screens/dashboard/Dashboard";
+import NoPageFound from "../screens/404/404";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <PrivateRoute path="/dashboard" component={Dashboard} />
+                <Route component={NoPageFound} />
             </Switch>
         </BrowserRouter>
     );
---
+++ client/src/screens/404/404.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Grid, Image, Message } from "semantic-ui-react";
+
+import "../../assets/css/404.css";
+
+import user from "../../apis/user";
+
+const NoPageFound = () => {
+    const history = useHistory();
+
+    const handleClickToReturn = () =>
+        user()
+            .then(() => history.push("/dashboard"))
+            .catch(() => history.push("/"));
+
+    return (
+        <Grid verticalAlign="middle" textAlign="center">
+            <Grid.Row className="pageContent">
+                <Grid.Column>
+                    <Image src="/img/404.png" size="large" centered />
+                    <Message
+                        floating
+                        header="404"
+                        content="Page not found - Click here to return"
+                        compact
+                        color="orange"
+                        size="large"
+                        onClick={() => handleClickToReturn()}
+                    />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
+};
+
+export default NoPageFound;
