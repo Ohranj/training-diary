6848265ac475ddcbe268ae1d1529668d6455faed
Ohranj <ajdorrington@hotmail.co.uk>
2021-01-24 19:14:27 +0000

    Tidied ui for deleting exercise

--- client/src/screens/dashboard/EditEntryExercises.js
+++ client/src/screens/dashboard/EditEntryExercises.js
@@ -3,12 +3,18 @@ import { Table, Icon } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { handleDeleteExercise } from "../../actions/entries";
 
+import SuccessMessage from "../../components/SuccessMessage";
+
 const EditEntryExercises = ({ dispatchDeleteExercise, entry }) => {
     const [currentEntry, setCurrentEntry] = useState(null);
+    const [showDeleteMsg, setShowDeleteMsg] = useState(false);
+    const [row, setRow] = useState(null);
 
     useEffect(() => {
         setCurrentEntry(entry);
     }, [entry, currentEntry]);
+
+    const checkActiveRow = (i) => (row === i ? setRow(null) : setRow(i));
 
     const handleDelete = (id, i) => {
         dispatchDeleteExercise({
@@ -19,44 +25,71 @@ const EditEntryExercises = ({ dispatchDeleteExercise, entry }) => {
             ...prev,
             exercises: prev.exercises.splice(i, 1),
         }));
+        resetDisplay();
+    };
+
+    const resetDisplay = () => {
+        setRow(null);
+        setShowDeleteMsg(true);
+        setTimeout(() => {
+            setShowDeleteMsg(false);
+        }, 1500);
     };
 
     return (
-        <Table compact celled size="small" textAlign="center" attached="top">
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>Exercise</Table.HeaderCell>
-                    <Table.HeaderCell>Sets</Table.HeaderCell>
-                    <Table.HeaderCell>Reps</Table.HeaderCell>
-                    <Table.HeaderCell>Weight</Table.HeaderCell>
-                    <Table.HeaderCell></Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {currentEntry
-                    ? currentEntry.exercises.map(
-                          ({ exercise, sets, reps, weight, id }, i) => (
-                              <Table.Row key={id}>
-                                  <Table.Cell content={exercise} />
-                                  <Table.Cell content={sets} />
-                                  <Table.Cell content={reps} />
-                                  <Table.Cell content={weight} />
-                                  <Table.Cell
-                                      content={
-                                          <Icon
-                                              name="trash"
-                                              onClick={() =>
-                                                  handleDelete(id, i)
+        <div>
+            {showDeleteMsg ? <SuccessMessage text="Exercise deleted" /> : null}
+            <Table compact celled size="small" textAlign="center">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Exercise</Table.HeaderCell>
+                        <Table.HeaderCell>Sets</Table.HeaderCell>
+                        <Table.HeaderCell>Reps</Table.HeaderCell>
+                        <Table.HeaderCell>Weight</Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {currentEntry
+                        ? currentEntry.exercises.map(
+                              ({ exercise, sets, reps, weight, id }, i) => (
+                                  <Table.Row key={id}>
+                                      <Table.Cell content={exercise} />
+                                      <Table.Cell content={sets} />
+                                      <Table.Cell content={reps} />
+                                      <Table.Cell content={weight} />
+                                      <Table.Cell
+                                          content={
+                                              <Icon
+                                                  name="trash"
+                                                  onClick={() =>
+                                                      checkActiveRow(i)
+                                                  }
+                                              />
+                                          }
+                                      />
+                                      {row === i ? (
+                                          <Table.Cell
+                                              content={
+                                                  <Icon
+                                                      name="check"
+                                                      onClick={() =>
+                                                          handleDelete(id, i)
+                                                      }
+                                                  />
                                               }
                                           />
-                                      }
-                                  />
-                              </Table.Row>
+                                      ) : (
+                                          <Table.Cell />
+                                      )}
+                                  </Table.Row>
+                              )
                           )
-                      )
-                    : null}
-            </Table.Body>
-        </Table>
+                        : null}
+                </Table.Body>
+            </Table>
+        </div>
     );
 };
 
