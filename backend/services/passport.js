0837b8b8c6ad77164afce003bc67f10fa672a05b
Ohranj <ajdorrington@hotmail.co.uk>
2021-01-26 21:51:17 +0000

    Added blurb

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -17,8 +17,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_SECRET,
-            callbackURL:
-                "https://training-diary.herokuapp.com/login/google/callback",
+            callbackURL: "/login/google/callback",
             proxy: true,
         },
         async (accessToken, refreshToken, profile, done) => {
