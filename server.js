d9405abb4bde211217226bbbf1c9fd4105b170d8
Ohranj <ajdorrington@hotmail.co.uk>
2021-01-26 21:03:23 +0000

    Added blurb

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const authRoutes = require("./backend/routes/auth");
 const entryRoutes = require("./backend/routes/entries.js");
 
 //Middleware
-app.use("/img", express.static(path.join(__dirname + "/backend/assets")));
+app.use("/img", express.static(path.join(__dirname, "backend/assets")));
 app.use(
     session({
         secret: process.env.sessionKey,
