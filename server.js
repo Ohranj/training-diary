a576163897155d3677194a875480f7b93bee683a
Ohranj <ajdorrington@hotmail.co.uk>
2021-01-27 10:41:06 +0000

    Added blurb

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const jsonParser = bodyParser.json();
 
-//Heroku hosting
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname + "/client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
-
 //Services
 require("./backend/services/mongoDB");
 require("./backend/services/passport");
@@ -44,5 +36,13 @@ app.use(passport.session());
 //Serve routes
 app.use("/", jsonParser, authRoutes);
 app.use("/entry", jsonParser, entryRoutes);
+
+//Heroku hosting
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
